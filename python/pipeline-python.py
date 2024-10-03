993a3885b08c3fc3861b1adef15d92a4bdb55cba
Cairo Faleiros <cairofal@gmail.com>
2024-10-03 00:37:10 -0300

    first commit

---
+++ python/pipeline-python.py
@@ -0,0 +1,11 @@
+from sklearn.pipeline import Pipeline  # type: ignore
+from pandas import read_csv, read_excel, read_json
+
+
+#Pipeline that extracts required features from a dataset then plots in a model
+
+#Data load into global variables
+
+#Creating features
+
+#Pipeline created
